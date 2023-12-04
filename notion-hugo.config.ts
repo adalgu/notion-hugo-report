b1c3b36dd26579568fed707aea0f514737f79daf
adalgu <adalgu@gmail.com>
2023-12-04 18:32:17 +0900

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/kamoreport/Notion-DoIt-673b468d49c04911a80fcf0b04c13d36?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
